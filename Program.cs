4060fd5a29de5df3c3a0821fe84624e6e8320efc
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2023-11-01 20:16:40 +0500

    INFRA:Upload Project

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+// First
+int radius=Convert.ToInt32(System.Console.ReadLine());
+float pi=3.1415f;
+decimal yuza =(decimal)pi*radius*radius;
+float length=2*pi*radius;
+System.Console.WriteLine($"S={yuza}, L={length}");
+
+// Second
+
+System.Console.WriteLine("Enter a value: ");
+int val = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter a kurs: ");
+int kurs = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Result of convertation: {val*kurs}");
+
+// Third
+
+System.Console.WriteLine("Enter a year of your birth: ");
+int YearOfBirth = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"You have lived for {(2023-YearOfBirth)*365} days!");
